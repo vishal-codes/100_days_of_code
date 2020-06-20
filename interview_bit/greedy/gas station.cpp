e8d94d7115250117cb4f1a1210467c005318070c
Gyanendra Singh <icm2017005@iiita.ac.in>
2020-06-20 09:15:06 +0530

    Graphs Resources Added

---
+++ interview_bit/greedy/gas station.cpp
@@ -0,0 +1,19 @@
+int sumGas = 0;
+        int sumCost = 0;
+        int tank = 0;
+        int start =0;
+        for(int i=0;i<gas.size();i++)
+        {
+            sumGas +=  gas[i];
+            sumCost += cost[i];
+            
+            tank += gas[i]-cost[i];
+            if(tank<0)
+            {
+                tank=0;
+                start=i+1;
+            }
+        }
+        if(sumCost>sumGas)
+            return -1;
+        return start;
