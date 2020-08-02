f680c7604a9030ec8d9f8a1a0246bc6ae72068c5
Vishal-codes <icm2017005@iiita.ac.in>
2020-08-02 16:39:52 +0530

    commit

--- leetcode/Leetcode Problems/150.evaluate-reverse-polish-notation.cpp
+++ leetcode/Leetcode Problems/150.evaluate-reverse-polish-notation.cpp
@@ -7,8 +7,59 @@
 // @lc code=start
 class Solution {
 public:
-    int evalRPN(vector<string>& tokens) {
-        
+    int value(string num)
+    {
+    
+        if(num[0]=='-')
+        {
+            return -stoi(num.substr(1));
+
+        }
+        return stoi(num);
+    }
+    int evalRPN(vector<string>& t) {
+        stack<int> s;
+
+        for(int i=0;i<t.size();i++)
+        {
+            if(t[i]=="+")
+            {
+                int a = s.top();
+                s.pop();
+                int b = s.top();
+                s.pop();
+                s.push(a+b);
+            }
+            else if(t[i]=="-")
+            {
+                int a = s.top();
+                s.pop();
+                int b = s.top();
+                s.pop();
+                s.push(b-a);
+            }
+            else if(t[i]=="*")
+            {
+                int a = s.top();
+                s.pop();
+                int b = s.top();
+                s.pop();
+                s.push(b*a);
+            }
+            else if(t[i]=="/")
+            {
+                int a = s.top();
+                s.pop();
+                int b = s.top();
+                s.pop();
+                s.push(b/a);
+            }
+            else
+            {
+                s.push(value(t[i]));
+            }
+        }
+        return s.top();
     }
 };
 // @lc code=end
