a5dae068de416e2b500284fbeb62cae0d3d8825e
Vishal-codes <icm2017005@iiita.ac.in>
2020-08-02 17:11:05 +0530

    clone graph

--- leetcode/Leetcode Problems/133.clone-graph.cpp
+++ leetcode/Leetcode Problems/133.clone-graph.cpp
@@ -9,7 +9,7 @@
 // Definition for a Node.
 class Node {
 public:
-    int val;
+    int val;1
     vector<Node*> neighbors;
     
     Node() {
@@ -31,8 +31,25 @@ public:
 
 class Solution {
 public:
+    Node* helper(Node* node,unordered_map<int,Node*>& m)
+    {
+        if(m.find(node->val)!=m.end())
+        {
+            return m[node->val];
+        }
+        Node* newNode = new Node(node->val);
+        m[node->val]=newNode;
+        for(int i=0;i<node->neighbors.size();i++)
+        {
+            newNode->neighbors.push_back(helper(node->neighbors[i],m));
+        }
+        return newNode;
+    }
     Node* cloneGraph(Node* node) {
-        return node;   
+         if(node==NULL)
+         return NULL;
+          unordered_map<int,Node*> m;
+          return helper(node,m);
     }
 };
 // @lc code=end
