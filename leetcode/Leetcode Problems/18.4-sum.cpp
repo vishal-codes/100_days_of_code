fb639dc91b33c50899cff0890196ac4fd9393723
Vishal-codes <icm2017005@iiita.ac.in>
2020-08-06 07:31:54 +0530

    pushing pending commits

---
+++ leetcode/Leetcode Problems/18.4-sum.cpp
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=18 lang=cpp
+ *
+ * [18] 4Sum
+ *
+ * https://leetcode.com/problems/4sum/description/
+ *
+ * algorithms
+ * Medium (33.60%)
+ * Likes:    2062
+ * Dislikes: 337
+ * Total Accepted:    338.6K
+ * Total Submissions: 1M
+ * Testcase Example:  '[1,0,-1,0,-2,2]\n0'
+ *
+ * Given an array nums of n integers and an integer target, are there elements
+ * a, b, c, and d in nums such that a + b + c + d = target? Find all unique
+ * quadruplets in the array which gives the sum of target.
+ * 
+ * Note:
+ * 
+ * The solution set must not contain duplicate quadruplets.
+ * 
+ * Example:
+ * 
+ * 
+ * Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+ * 
+ * A solution set is:
+ * [
+ * ⁠ [-1,  0, 0, 1],
+ * ⁠ [-2, -1, 1, 2],
+ * ⁠ [-2,  0, 0, 2]
+ * ]
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        
+    }
+};
+// @lc code=end
+
